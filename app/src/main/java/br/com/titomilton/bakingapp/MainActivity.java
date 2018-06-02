a0665dde32428d19b438a2188923369f43c86a74
Milton Siqueira <miltonsiqueira@gmail.com>
2018-06-02 15:33:05 -0400

    Layout list recipes

--- app/src/main/java/br/com/titomilton/bakingapp/MainActivity.java
+++ app/src/main/java/br/com/titomilton/bakingapp/MainActivity.java
@@ -1,13 +1,18 @@
 package br.com.titomilton.bakingapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RecipeListFragment.OnListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onListFragmentInteraction(Recipe recipe) {
+
     }
 }
---
+++ app/src/main/java/br/com/titomilton/bakingapp/RecipeRecyclerViewAdapter.java
@@ -0,0 +1,73 @@
+package br.com.titomilton.bakingapp;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import br.com.titomilton.bakingapp.RecipeListFragment.OnListFragmentInteractionListener;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link Recipe} and makes a call to the
+ * specified {@link OnListFragmentInteractionListener}.
+ */
+public class RecipeRecyclerViewAdapter extends RecyclerView.Adapter<RecipeRecyclerViewAdapter.ViewHolder> {
+
+    private final List<Recipe> mValues;
+    private final OnListFragmentInteractionListener mListener;
+
+    public RecipeRecyclerViewAdapter(List<Recipe> items, OnListFragmentInteractionListener listener) {
+        mValues = items;
+        mListener = listener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_recipe_list_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        Recipe recipe = mValues.get(position);
+        holder.mItem = recipe;
+        holder.mContentView.setText(recipe.getName());
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != mListener) {
+                    // Notify the active callbacks interface (the activity, if the
+                    // fragment is attached to one) that an item has been selected.
+                    mListener.onListFragmentInteraction(holder.mItem);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final TextView mContentView;
+        public Recipe mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            mContentView = view.findViewById(R.id.content);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
